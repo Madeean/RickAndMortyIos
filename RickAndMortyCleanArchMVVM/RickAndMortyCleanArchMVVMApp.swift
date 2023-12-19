40102cbdc7d19e76cfca5744cbe511e014fcdfca
MadeReihan <reihanmade@gmail.com>
2023-12-19 13:28:33 +0700

    Initial Commit

---
+++ RickAndMortyCleanArchMVVM/RickAndMortyCleanArchMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyCleanArchMVVMApp.swift
+//  RickAndMortyCleanArchMVVM
+//
+//  Created by made reihan on 19/12/23.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyCleanArchMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
