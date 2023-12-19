91818aa1bdfd3d7ffcb8a6aeeca0653a5db81d2c
MadeReihan <reihanmade@gmail.com>
2023-12-19 21:29:48 +0700

    loading load image

--- RickAndMortyCleanArchMVVM/View/ContentView.swift
+++ RickAndMortyCleanArchMVVM/View/ContentView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import SDWebImageSwiftUI
+import SDWebImage
 
 struct ContentView: View {
     @ObservedObject private var viewModel = ListViewModel()
@@ -52,10 +53,16 @@ struct ItemListImageAndName: View {
             HStack(alignment: .top, spacing: 20) {
                 
                 if item.image != "" {
-                    WebImage(url: URL(string: item.image)).resizable().frame(width: 100, height: 100).cornerRadius(20)
+                    AnimatedImage(
+                        url: URL(string: item.image))
+                    .resizable()
+                    .indicator(SDWebImageActivityIndicator.medium)
+                    .frame(width: 100, height: 100)
+                    .cornerRadius(20)
                 }else{
                     Image("NoImage").resizable().frame(width: 100, height: 100).cornerRadius(20)
                 }
+                
                 VStack(alignment: .leading, spacing: 20) {
                     Text(item.name).bold().font(.title2)
                     Text("\(item.species) - \(item.species)")
